b37e901affbc6d0546de4a7807fcb522760cca1f
codingreaction <codingreaction@gmail.com>
2024-12-07 00:06:41 -0300

    updated nextjs to v15 and nodejs wip

--- next.js
+++ next.js
@@ -1,52 +1,135 @@
-// CLI
-npx create-next-app@latest .
+//////  CLI
+> npx create-next-app@latest .
+> pnpm run dev
+//////  SERVER COMPONENTS vs CLIENT COMPONENTS
+- build on server exclusively
+- no effects/hooks
+- faster page loads
+- default
+- [server] tag, logs on TERMINAL
+vs
+- use client
+- browser apis
+///////// ROUTING
+users/page.tsx
+users/[id]/page.tsx
 
-//Routing and Navigation
-// src/app --> all pages
-
-page.tsx            // "/"
-posts/[id]/page.tsx // "posts/3/"
-
-// layout.tsx contains <Header /> { children } <Footer /> // children: React.ReactNode
-
-
+function Users({params}: {params: {id: string}}){
+  const { id } = await params;
+  return <h1>Product: {id}</h1>;
+}
+/////// PAGE GROUPS
+(auth)/login/page.tsx    // login/ route, logical grouping
+(auth)/register/page.tsx // only matters for organizing
+////// METADATA
 export const metadata:Metadata = { // inside layout.tsx
   title: "Create Next App",
   description: "Description of site"
 };
-
-// COMPONENTS
-<Link href={...}> // client side <a href...>
-<Image src="..." width="50" height="50" />
-
-export default async function MyServerComponent({params}){
-    const response = await fetch(...);
-    const data = await response.json();
-
-    return <h1>...</h1>
+/////// LAYOUTS: shared between multiple pages
+src/app/layout.tsx -> basic layout for all pages
+function RootLayout({children}: Readonly<{children: React.ReactNode;}>){
+  return (
+    <html lang="en">
+      <body> {children} </body>
+    </html>
+  )
 }
 
-// SERVER ACTIONS
-const addPost = async (formData: FormData) => {
-  "user server";
+products/[id]/layout.tsx -> applies for all product pages
+function ProductLayout({children}: {children: React.ReactNode}){
+  return <div>
+  	  <h2>Featured</h2>
+  	  {children}
+	</div>
+}
+////// LINK and ROUTER
+<Link href="/">Home</Link> // uses nextjs router for navigation
+import {usePathname, useRouter} from "next/navigation"
+const pathname = usePathname(); // pathname === "/" "/about"
+const router = useRouter();
+router.push("/") // navigate programatically
+//////////// ROUTE HANDLERS (api)
+src/app/users/route.ts
 
-  await prisma.post.create({data: {title: formData.get("title") as string,}});
-  revalidatePath("/posts"); // rerun the posts route
+export async function GET(){
+  return Response.json(users);
 }
 
-return <form action={addPost}> </form>
+export async function POST(request: Request){
+  const user = await request.json();
+  const newUser = createUser({id: 1, name: user.name});
+  return new Response(JSON.stringify(newUser,{
+    headers: {
+      "Content-Type": "application/json"
+    },
+    status: 201,
+  });
+}
 
-// inside the SubmitBtn component of the form
-const {pending} = useFormStatus();
+/users/[id]/route.ts
 
-<button type="submit" disabled={pending} />
+export async function GET(_request: Request, ctx: {params}: {params:{id:string}}){
+  const {id} = await params;
+  const user = users.find(user => user.id === id);
+  return Response.json(user);
+}
 
-// SUSPENSE AND STREAMING
+export async function PATH(){...}
 
+///////// DATA FETCHING
+// in server components you can users = await fetch('...') directly
+// in client components only in useEffect
+
+//////////// LOADING
+app/users-server/loading.tsx
+export default function Loading(){
+  return <div> Loading users...</div>
+}
+
+app/users-server/error.tsx
+"use client"
+export default function Error({error}: {error: Error}){
+  return <div>error fetching data </div>
+}
+//////////// SERVER ACTIONS: async functions on server, form, db, etc
+app/mock-users/page.tsx
+
+import {revalidatePath} from "next/cache";
+
+async function MockUsers(){
+  const res = await fetch("mockusers-endpoint");
+  const users = await res.json();
+
+  async function addUser(formData: FormData){
+    "use server"
+    const name = formData.get("name")
+    const res = await fetch(
+      "https://users",
+      {
+	method: "POST",
+      	headers: {
+        	"Content-Type": "application/json",
+		Authorization: "Bearer PRIVATE_KEY",
+      	},
+      	body: JSON.stringify({name}),
+      }
+     );
+     const newUser = await res.json();
+     revalidatePath("/mock-users");
+     console.log(newUser);
+  }
+  return <div>
+    <form action={addUser}>
+      <input type="text" name="name" required />
+      <button> Add user </button>
+   </form>
+    <div>{users.map()}</div>
+  </div>
+////////// SUSPENSE AND STREAMING
 export const dynamic = 'force-dynamic'; // disable component exported as static in order to run client interaction
 await fetch(...,{ cache: "no-cache" }); // {next: { revalidate: 3600 }}
-//
-// MIDDLEWARE
+////////// MIDDLEWARE
 app/middleware.ts
 
 export function middleware(request: NextRequest){
@@ -60,7 +143,109 @@ export function middleware(request: NextRequest){
 export const config = {
   matcher: ["/dashboard", "/account"];
 }
+//////// PAGINATION
 
-// React Query
+////////// CACHE
 
-// Zustand
+///////// AUTH
+
+/*************************** ROUTER (WOUTER) *****************************/
+<Link href="/users/1">Profile</Link>
+<Redirect to="/logout" />
+
+//////// COMPONENTS API ///////////////////
+<Switch>
+  <Route path="/about">About us</Route>
+  <Route path="/contact"> <ContactForm /> </Route>
+  <Route path="/users/:name">{(params)=> <div>Hello, {params.name}!</div></Route>
+  <Route path="/profile" component={ProfilePage} />
+  <Router base="/job">
+    <Route path="/payments" component={PaymentsPage} />
+  </Router>
+  <Route> Rendered if anything matches </Route>
+</Switch>
+
+//////// HOOKS API //////////////
+const [match, params] = useRoute("/users/:id");
+const [location, setLocation] = useLocation(); // setLocation("/somewhere"), location = current page
+const params = useParams(); //params.id for path="/user/:id"
+navigate("/new-route") // navigate("/home", {replace: true});
+
+/************************** ZUSTAND *************************************/
+import {create} from 'zustand';
+
+const useProfileStore = create(set => ({
+  name: "Max",
+  age: 33,
+  changeName: (newName) => set(state => ({name: newName})),
+  increaseAge: () => set(state=> ({age: state.age + 1})),
+  unname: () => set({name: ""}),
+}));
+
+const completeState = useProfileStore();
+
+const name = useProfileStore(state => state.name);
+<p>{name}</p>
+
+const changeName = useProfileStore(state => state.changeName);
+<button onClick={()=> changeName("Pedro")}>Rename </button>
+
+/************************* REACT QUERY ********************************/
+const queryClient = new QueryClient();
+
+<QueryClientProvider client={queryClient}>
+  <App />
+</QueryClient>
+
+const [search, setSearch] = useState("");
+
+const {data: todos, loading} = useQuery({
+  queryFn: ()=> fetchDataFn(search),
+  queryKey: ["todos", {search}],
+  staleTime: Infinity, //or a valid numb of seconds. Don't refetch on mount if cached data exists
+  cacheTime: 0, // remove caching functionallity
+});
+
+const queryClient = useQueryClient();
+
+const {mutate, mutateAsync: addTodoMutation} = useMutation({
+  mutationFn: addTodo,
+  onSuccess: ()=>{
+    queryClient.invalidateQueries(["todos"]);
+  },
+});
+
+if (isLoading) return <div>Loading...</div>
+<button onClick={async (evt) => await addTodoMutation({title})}>Add todo </button>
+
+/************************ JEST **********************************/
+import {render, fireEvent} from "@testing-library/react";
+import Counter from "./MyComponent";
+
+beforeAll(()=> recreateDb());
+afterAll(()=> clearDb());
+beforeEach(()=>...);
+afterEach(()=>...);
+
+describe(Counter, () =>{
+  it("check initial value", () => {
+    const initialValue = 0;
+    const {getByTestId} = render(<Counter initial={initialValue} />);
+    const count = getByTestId("count").innerText; // <p data-testid="count"> {{ count }} </p>
+    expect(count).toEqual(initialValue);
+  });
+
+  it("check if button works", () => {
+    const initialValue = 0;
+    const {getByTestId, getByRole} = render(<Counter initial={initialValue} />);
+    const button = getByRole("button", {name: "Increment"}); // <button> Increment </button>
+    fireEvent.click(button);
+    const count = getByTestId("count").innerText;
+    expect(count).toEqual(initialValue);
+  });
+});
+
+//////// REACT QUERY
+
+//////// Zustand
+
--- nodejs-cheatsheet.js
+++ nodejs-cheatsheet.js
@@ -11,6 +11,7 @@ cors
 bcrypt
 jsonwebtoken
 express-validator
+pm2
 
 
 ///////////////////// FS   //////////////////////////////////////////
@@ -41,6 +42,7 @@ http.request('path', (res)=>{
 
 res.setHeader('Content-Type', 'text/html');
 res.write('<html>...</html>');
+res.json({name: "Max"});
 res.statusCode = 300
 res.end();
 
@@ -76,6 +78,10 @@ app.use('/static', express.static(path.join(__dirname, 'public'));
 
 app.use(express.static('public')); 	      // localhost:8000/images/cat.png
 app.use('/static', express.static('public')); // localhost:8000/static/images/cat.png
+
+////////////////// TEMPLATING HTML5 ////////////////////
+ app.set('view engine', 'ejs');
+ app.get(..., ()=> res.render("index", {name: "Max"})); // <%= locals.text || 'default' %>
 
 ////////////////// ROUTER /////////////////////////////////////
 
@@ -114,7 +120,7 @@ app.use('/birds', birdsRouter);
 - res.send('ANYTHING: json, Buffer.from("bla"), text/html, text/plain')// previously call res.set('Content-Type', 'text/html')
 - res.format({            // content-negociation bassed on Accept HTTP header from request
     'text/plain': function() { res.send('hey') },
-    'application/json': function() { res.send({message: 'hey'})}, 
+    'application/json': function() { res.send({message: 'hey'})},
     default: function() { res.status(406).send('Not acceptable') },
   });
 
@@ -130,17 +136,19 @@ app.use('/birds', birdsRouter);
 
 //////////////// MIDDLEWARES //////////////////////
 app.use(morgan("combined"));
-app.use(express.json()); // parse uncomming JSON requests
 app.use((req, res, next) => { console.log('Logger'); re.requestTime = Date.now(); next(); }); // Logger + request time
 app.use(cookieParser()); // cookieParser = require('cookie-parser')
 app.use(async (req, res, next) => await validator(req.cookies); next() });
-
+app.use(express.json()); //parse the body on req, req.body
+app.use(express.urlenconded({extended: true}); //info that comes from forms
 /////////////// LOGGER ///////////////////////////////
 
 ///////////////// READ FILES, CSV, JSON ////////////////////////////////////
 req.files.avatar
 
 ///////////////// ENV /////////////////////////////////////////
+const dotEnv = require("dotenv").config();
+const PORT = process.env.PORT;
 
 //////////////// SESSIONS COOKIES ///////////////////////////
 
@@ -150,7 +158,6 @@ const cors = require('cors');
 app.use(cors({ origin: ['http://localhost:5071/'], credentials: true, methods: ["GET", "POST"]}));
 
 ///////////////// TESTING ////////////////////////////
-///////////////// PRISMA //////////////////////////////////////
 
 //////////////// MONGO DB / MONGOOSE ////////////////////////
 const {MongoClient, ServerApiVersion } = require("mongodb");
@@ -212,18 +219,109 @@ bcrypt.compare(password, userStore.password);
 
 req.headers.authorization // to fetch the "Bearer TOKEN"
 
+ const bcrypt = require("bcrypt");
+ const hashedPassword = await bcrypt.hash(rawPassword, 10);
+ //jsonwebtoken package
+ const jwt = require("jsonwebtoken");
+ if (user && (await bcrypt.compare(rawPassword, user.hashedPass))
+   access = jwt.sign(
+	   {id: user.id, email: user.email},
+	   process.env.TOKEN_SECRET,
+   	   {expiresIn: "1m"})
+   return res.status(200).json({access});
+
+   const validateToken = asyncHandler(async(req, res, next) =>
+   let token;
+   let authHeader = req.headers.Authorization || req.headers.authorization;
+   if (authHeader && authHeader.startsWith("Bearer")){
+     token = authHeader.split(" ")[1];
+     jwt.verify(token, process.env.TOKEN_SECRET, (err, decoded) =>{
+	     if (err) thrown new Error("user not authorized");
+	     req.user = decoded.user
+	     next();
+     });
+    }
 
 //////////////// PASSPORT JS /////////////////////////////////
-	
+
 //////////////// GRAPH QL //////////////////////////////////
-	
+
 //////////////// TRPC    //////////////////////////////////
 
 //////////////// ZOD //////////////////////////////////////
-	
+https://dev.to/osalumense/validating-request-data-in-expressjs-using-zod-a-comprehensive-guide-3a0j
+
 //////////////// SOCKETS    /////////////////////////////////
 IP SOCKETS
 DATAGRAM SOCKETS
 TCP SOCKETS
 WEB SOCKETS
 
+//////////////// NODEJS EVENT LOOP OPTIMIZATION /////////////
+
+//////////////// DRIZZLE ORM /////////////////////////
+packages: @types/node, tsx, typescrit
+
+> npm i drizzle-orm postgres
+> npm i -D drizzle-kit
+
+> npx drizzle-kit generate:pg // generate migration file
+> npx drizzle-kit generate:drop
+
+#drizzle.config.ts
+import {defineConfig} from "drizzle-kit";
+
+export default defineConfig({
+  schema: "./src/drizzle/schema.ts",
+  out: "./src/drizzle/migrations",
+  driver: "pg",
+  dbCredentials: {
+    connectionString: process.env.DATABASE_URL as string
+  },
+  verbose: true,
+  strict: true
+});
+
+#schema.ts
+import {pgTable, uuid, serial, varchar} from "drizzle-orm/pg-core";
+
+export const UserRole = pgEnum("userRole", ["ADMIN", "BASIC"]);
+
+export const UserTable = pgTable("user",{
+  id:  uuid("id").primaryKey().defaultRandom(),
+  id2: serial("id2").primaryKey(),
+  name: varchar("name", {length: 255}).notNull(),
+  age: integer("age").notNull(),
+  email: varchar("email", {length: 255}).notNull().unique(),
+  role: UserRole("userRole").default("BASIC").notNull(),
+  userProfile: uuid("profileId").references(() => ProfileTable.id).notNul();
+}, table => {
+				return {
+				   emailIndex: index("emailIndex").on(table.email),
+				   uniqueNameAndAge: unique("uniqueNameAndAge").on(table.name, table.age),
+
+  // supose a category table for many to many
+  //
+				   //
+const PostCategoryTable = pgTable("post-category", {
+  postId: ...,
+  categoryId: ...,
+}, table => ({pk: primaryKey({ columns: table.postId, table.categoryId})});
+
+#migrate.ts // code for running the migrations
+import "dotenv/config"
+import {drizzle} from 'drizzle-orm/postgres-js';
+import {migrate} from 'drizzle-orm/postgres-js/migrator';
+import postgres from 'postgres';
+
+// create a command to run this file
+const migrationClient = postgres(PG_URL) // RUN a migration
+await migrate(drizzle(migrationClient), {migrationFolder: '');
+await migrateClient.end();
+
+const queryClient = postgres(PG_URL); // RUN a query
+const db = drizzle(queryClient, { schema, logger: true});
+await db.select().drom(...)
+
+await db.insert(UserTable).values({name: "max"});
+await db.query.UserTable
